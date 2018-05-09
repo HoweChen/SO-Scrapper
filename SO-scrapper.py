fd1b7e3548e76be935bfaf5f7f186021e500315b
SkyFather <cyh278889789@gmail.com>
2018-05-09 15:26:23 +0800

    update with SO-Scrapper

--- SO-scrapper.py
+++ SO-scrapper.py
@@ -6,6 +6,7 @@ from threading import Thread
 import queue
 import time
 import sqlite3
+import sys
 
 global url_pool
 # url_pool = ["https://stackoverflow.com/questions/tagged/python?page=" + str(i) + "&sort=votes&pagesize=15" for i in
@@ -64,11 +65,11 @@ def sub_url_analysis(sub_url):
     question_name = bs_obj.find("a", {"class": "question-hyperlink"}).get_text()
     favorite_count = int(bs_obj.find('div', {'class': 'favoritecount'}).get_text())
     question_vote = int(votes[0].get_text())
-    if len(votes) >= 1:
-        answer_vote = votes[1].get_text()
+    if len(votes) > 1:
+        answer_vote = int(votes[1].get_text())
     else:
         answer_vote = None
-    answer_text = bs_obj.findAll('div', {'class': 'post-text', 'itemprop': 'text'})[1].get_text
+    answer_text = str(bs_obj.findAll('div', {'class': 'post-text', 'itemprop': 'text'})[1].get_text)
     return_dict = {
         'question_id': question_id,
         'question_name': question_name,
@@ -77,60 +78,63 @@ def sub_url_analysis(sub_url):
         'question_vote': question_vote,
         'answer_text': answer_text
     }
+    # for item, value in return_dict.items():
+    #     print("{0}: {1}".format(item, type(value)))
     questions_list_dict.append(return_dict)
 
 
 def data_store(question_dict):
     conn = sqlite3.connect('SO-Python.db')
     c = conn.cursor()
-    c.execute('insert into SOPython values (?,?,?)',
-              [question_dict['question_id'], question_dict['question_name'], question_dict['favorite_count'],
-               question_dict['question_vote'], question_dict['answer_vote'], question_dict['answer_text']])
-    conn.commit()
-    print('Insert Successfully')
+    try:
+        c.execute('insert into SOPython values (?,?,?,?,?,?)',
+                  [question_dict.get('question_id'), question_dict.get('question_name'),
+                   question_dict.get('favorite_count'),
+                   question_dict.get('question_vote'), question_dict.get('answer_vote'),
+                   question_dict.get('answer_text')])
+        print('---------------------')
+        conn.commit()
+        print('Insert Successfully')
+    except Exception as e:
+        print(e)
+        print('Insertion Failed')
     conn.close()
 
 
 def url_fetch_entry():
     global url_pool
-    with ThreadPool(processes=5) as pool:
+    with ThreadPool(processes=10) as pool:
         pool.map(url_fetch, url_pool)
     global sub_url_pool
     for item in sub_url_pool:
         print(item)
+    print(len(sub_url_pool))
 
 
 def sub_url_analysis_entry():
     global sub_url_pool
-    if not sub_url_pool:
-        return
-    else:
-        with ThreadPool(processes=10) as pool:
-            pool.map(sub_url_analysis, sub_url_pool)
+
+    with ThreadPool(processes=10) as pool:
+        pool.map(sub_url_analysis, sub_url_pool)
 
 
 def data_store_entry():
     global questions_list_dict
-    if questions_list_dict:
-        with ThreadPool(processes=10) as pool:
-            pool.map(data_store, questions_list_dict)
+    with ThreadPool(processes=10) as pool:
+        pool.map(data_store, questions_list_dict)
 
 
 def main():
-    # url_fetch_process = Process(target=url_fetch_entry)
-    # sub_url_analysis_process = Process(target=sub_url_analysis_entry)
-    # data_store_process = Process(target=data_store_entry)
-    # url_fetch_process.start()
-    # sub_url_analysis_process.start()
-    # data_store_process.start()
-    # url_fetch_process.join()
-    # sub_url_analysis_process.join()
-    # data_store_process.join()
-    # url_fetch_entry()
-    # sub_url_analysis_entry()
-    url_fetch_entry()
-    sub_url_analysis_entry()
-    data_store_entry()
+    fetch_thread = Thread(target=url_fetch_entry)
+    analysis_thread = Thread(target=sub_url_analysis_entry)
+    store_thread = Thread(target=data_store_entry)
+
+    fetch_thread.start()
+    fetch_thread.join()
+    analysis_thread.start()
+    analysis_thread.join()
+    store_thread.start()
+    store_thread.join()
 
 
 if __name__ == '__main__':
